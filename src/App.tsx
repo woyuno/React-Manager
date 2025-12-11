4dba08ef0d2594c674cbab83ef1fcfec85423dbc
武晋 <452501979@qq.com>
2025-12-11 12:16:46 +0800

    feat: 主题色设置

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router/router'
+import { ConfigProvider } from 'antd'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ConfigProvider theme={{token:{
+      colorPrimary:'#00b96b'
+    }}}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ConfigProvider>
   )
   // return <RouterProvider router={router} />
 }
--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,18 +1,18 @@
-import './Login.less'
 import { Form, Input, Button } from 'antd'
+import style from './Login.module.less'
 export default function Login() {
   const onFinish = () => {}
   return (
-    <div className='login'>
-      <div className='login-wrapper'>
-        <div className='title'>系统登录</div>
+    <div className={style.login}>
+      <div className={style.loginWrapper}>
+        <div className={style.title}>系统登录</div>
         <Form name='basic' initialValues={{ remember: true }} onFinish={onFinish} autoComplete='off'>
-          <Form.Item name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
-            <Input size='large'/>
+          <Form.Item name='username' rules={[{ required: true, message: '请输入账号' }]}>
+            <Input size='large' />
           </Form.Item>
 
-          <Form.Item name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
-            <Input.Password  size='large'/>
+          <Form.Item name='password' rules={[{ required: true, message: '请输入密码' }]}>
+            <Input.Password size='large' />
           </Form.Item>
 
           <Form.Item>
